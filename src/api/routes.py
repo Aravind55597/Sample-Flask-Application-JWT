33627c524ec1697937208b399e1e3d05fe133389
Aravind55597 <gowthamaravindfaiz@gmail.com>
2022-09-01 20:45:09 +0800

    Updated with comments on app.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from api.utils import  APIException
 
 api = Blueprint('api', __name__)
 

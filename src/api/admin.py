8afe60a91311f53966dcd1c5d9d93e6febffcc7a
Aravind55597 <gowthamaravindfaiz@gmail.com>
2022-09-06 00:25:20 +0800

    Greatly simplified file

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,13 +1,13 @@
-  
 import os
 from flask_admin import Admin
-from .models import db, User
+from .User import  User
 from flask_admin.contrib.sqla import ModelView
+from .utils import db
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin = Admin(app, name='Sample Flask App JWT', template_mode='bootstrap3')
 
     
     # Add your models here, for example this is how we add a the User model to the admin
--- src/app.py
+++ src/app.py
@@ -1,55 +1,10 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
-from flask_migrate import Migrate
-from flask_cors import CORS
-from api.utils import APIException
-from api.models import db
-from api.routes import api
-from api.admin import setup_admin
-from api.commands import setup_commands
+from venv import create
+from api import create_app; 
+import os; 
 
-#from models import Person
-
-ENV = os.getenv("FLASK_ENV")
-app = Flask(__name__)
+app=create_app(); 
 
 
-# database configuration--------------------------------------------------------------------------------------------------------------------------------------
-db_url = os.getenv("DATABASE_URL")
-
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-MIGRATE = Migrate(app, db, compare_type = True)
-
-db.init_app(app)
-
-
-# Allow CORS requests to this API-----------------------------------------------------------------------------------------------------------------------------
-CORS(app)
-
-# add the admin-----------------------------------------------------------------------------------------------------------------------------------------------
-setup_admin(app)
-
-# set up ability to seed database-----------------------------------------------------------------------------------------------------------------------------
-setup_commands(app)
-
-# Add all endpoints from the API with a "api" prefix----------------------------------------------------------------------------------------------------------
-app.register_blueprint(api, url_prefix='/api')
-
-# Handle/serialize errors like a JSON object------------------------------------------------------------------------------------------------------------------
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
